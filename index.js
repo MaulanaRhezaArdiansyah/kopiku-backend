4a2a1ffa642afd4c9ec8a59495969d699c22ab3f
Maulana Rheza <103730296+MaulanaRhezaArdiansyah@users.noreply.github.com>
2023-03-12 11:08:19 +0800

    try again

--- index.js
+++ index.js
@@ -2,32 +2,25 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-// const port = 3000;
-const port = 3001;
+const port = process.env.PORT || 3001;
 const router = require("./src/routes/index");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
 const { urlencoded, json } = require("body-parser");
 
 // static file
 app.use(express.static("public"));
-
 // menerima application/x-www-form-urlencoded
 app.use(urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: true }));
-
 // menerima json
 app.use(json());
 // app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://kopikuu.vercel.app/");
-  next();
-});
-
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://kopikuu.vercel.app/");
+//   next();
+// });
 // CORS
 app.use(cors());
-
 // app.use(
 //   cors({
 //     origin: `http://localhost:3000/`
